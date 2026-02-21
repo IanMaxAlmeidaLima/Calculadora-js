ff2e6bb0201dbad2c4a5a322989bf765235c9f65
IanMaxAlmeidaLima <iancna360@gmail.com>
2026-02-20 21:45:46 -0300

    Feat: Adicionadas as funções de inserção de números na calculadora e a de limpar campo de operações

---
+++ src/javascript/calculadora.js
@@ -0,0 +1,8 @@
+function insert(num){
+    let numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+}
+
+function clean(){
+    document.getElementById('result').innerHTML = "";
+}
